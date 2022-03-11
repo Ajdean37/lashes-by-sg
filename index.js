0e549447ef0acc59cce6177f2802350afda0383c
Ajdean37 <ajdean37@gmail.com>
2022-03-11 09:19:41 -0600

    changes

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-function myBtn() {
-    let el = document.getElementById("bookBtn");
-    x.disabled = true;
-  }
+// function myBtn() {
+//     let el = document.getElementById("bookBtn").onclick;
+//     location.href = "www.facebook.com/lashesbyserenagene";
+//   }
+
+//   myBtn();
+
+
+
+
+//   document.getElementById("bookBtn").onclick = function () {
+//     location.href = "www.facebook.com/lashesbyserenagene";
+
+var button = document.querySelector('bookBtn');
+    button.onclick = function() {
+    window.location='https://www.facebook.com/lashesbyserenagene/';
+}
+
+var button = document.querySelector('appBtn');
+    button.onclick = function() {
+    window.location='https://www.facebook.com/lashesbyserenagene/';
+}
+
+
